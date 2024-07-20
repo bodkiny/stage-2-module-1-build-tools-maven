b92a3b841ab75faf8c196d5cba331dfb48cd7925
Bohdan Rudenko <brudenko2804@gmail.com>
2024-07-20 19:12:10 +0300

    Complete the task

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,16 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.math.BigDecimal;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (NumberUtils.isParsable(str)) {
+            if (str.startsWith("0")) return false;
+            BigDecimal number = new BigDecimal(str);
+            return number.compareTo(BigDecimal.ZERO) > 0;
+        }
+        return false;
     }
 }
